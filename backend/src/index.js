1fc7a14e815ad76f9a60e3176531e871ff7329fc
Saksham Miglani <saksham@Saksham-ka-MacBook-Pro.local>
2025-11-05 14:16:20 +0530

    cors link

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,10 @@ const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://campus-ride-gray.vercel.app/"],
+  credentials: true,
+}));
 app.use(express.json()); 
 
 connectDB(process.env.MONGO_URI);
