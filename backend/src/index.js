becf9353f491081a96d2cce2868330374dace4eb
Saksham Miglani <saksham@Saksham-ka-MacBook-Pro.local>
2025-11-05 13:24:18 +0530

    corrected port

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,5 +13,6 @@ connectDB(process.env.MONGO_URI);
 
 app.use('/api/auth', authRoutes); 
 
-const PORT = 5050;
+const PORT = process.env.PORT || 5050;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
